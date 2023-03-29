8315355e59ee522559416380b373e8d9a7762b48
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-03-29 22:33:12 +0300

    43

--- array.js
+++ array.js
@@ -4,29 +4,53 @@ const guesNumber = document.querySelector("#guess");
 const check = document.querySelector("#checkBtn");
 const again = document.querySelector("#again");
 let secretNumber = Math.ceil(Math.random() * 100);
-console.log(secretNumber);
-console.log(guesNumber);
-console.log(check);
-let remainigGuess = 5;
-check.onclick = () => {
-  remainigGuess--;
 
-  if (remainigGuess > 0) {
-    if (guesNumber.value < secretNumber) {
-      alert(`⬆️ kalan hak:${remainigGuess}`);
-    } else if (guesNumber.value > secretNumber) {
-      alert(`⬇️ kalan hak:${remainigGuess}`);
+console.log(secretNumber);
+
+let remainigGuess = 5;
+
+check.onclick = () => {
+  if (guesNumber >= 0 && guesNumber <= 100) {
+    remainigGuess--;
+
+    if (remainigGuess > 0) {
+      if (guesNumber.value < secretNumber) {
+        document.querySelector(
+          "#info"
+        ).textContent = `⬆️ kalan hak:${remainigGuess}`;
+        guesNumber.value = "";
+      } else if (guesNumber.value > secretNumber) {
+        document.querySelector(
+          "#info"
+        ).textContent = `⬇️ kalan hak:${remainigGuess}`;
+        guesNumber.value = "";
+      } else {
+        score.textContent = 2 * (remainigGuess + 1);
+        number.textContent = secretNumber;
+        document.querySelector("body").style.backgroundColor = "blue";
+        document.querySelector("#info").textContent = `Tebrikler 🏆 ${
+          5 - remainigGuess
+        } kerede bildiniz`;
+      }
     } else {
-      score.textContent = 2 * (remainigGuess + 1);
-      number.textContent = secretNumber;
-      alert(`Tebrikler 🏆 ${5 - remainigGuess} kerede bildiniz`);
+      document.querySelector("body").style.backgroundColor = "red";
+      alert("Hakkınız kalmadı");
+      remainigGuess.textContent = "5";
+      document.querySelector("#info").textContent = `⬇️ kalan hak:0`;
+      guesNumber.value = "";
+      guesNumber.textContent = "";
     }
-  } else alert("Hakkınız kalmadı");
+  } else {
+    alert("0 ve 100 arasında bir sayı giriniz");
+    guesNumber.value = "";
+  }
 };
 
 again.onclick = () => {
   number.textContent = "?";
   remainigGuess = 5;
   secretNumber = Math.ceil(Math.random() * 100);
-  console.log(secretNumber);
+  document.querySelector("#info").textContent = `kalan hak:5`;
+  document.querySelector("body").style.backgroundColor = "#d9ff91";
+  guesNumber.value = "";
 };
