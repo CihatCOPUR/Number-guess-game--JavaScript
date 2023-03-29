c69899480af1df8bd8ac24614c6011dd2cbbdd18
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-03-29 21:47:46 +0300

    asd

--- array.js
+++ array.js
@@ -1,10 +1,32 @@
-let secretNumber = document.querySelector("#secretNumber");
-
+let number = document.querySelector("#secretNumber");
+let score = document.querySelector("#score");
 const guesNumber = document.querySelector("#guess");
 const check = document.querySelector("#checkBtn");
-secretNumber = Math.ceil(Math.random() * 100);
+const again = document.querySelector("#again");
+let secretNumber = Math.ceil(Math.random() * 100);
 console.log(secretNumber);
 console.log(guesNumber);
 console.log(check);
+let remainigGuess = 5;
+check.onclick = () => {
+  remainigGuess--;
 
-check.onclick = () => {};
+  if (remainigGuess > 0) {
+    if (guesNumber.value < secretNumber) {
+      alert(`⬆️ kalan hak:${remainigGuess}`);
+    } else if (guesNumber.value > secretNumber) {
+      alert(`⬇️ kalan hak:${remainigGuess}`);
+    } else {
+      score.textContent = 2 * (remainigGuess + 1);
+      number.textContent = secretNumber;
+      alert(`Tebrikler 🏆 ${5 - remainigGuess} kerede bildiniz`);
+    }
+  } else alert("Hakkınız kalmadı");
+};
+
+again.onclick = () => {
+  number.textContent = "?";
+  remainigGuess = 5;
+  secretNumber = Math.ceil(Math.random() * 100);
+  console.log(secretNumber);
+};
