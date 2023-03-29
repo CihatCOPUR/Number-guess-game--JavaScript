5758b8d6bfe9831299fec0f30825bdd321c7141f
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-03-29 14:21:35 +0300

    21

--- array.js
+++ array.js
@@ -1,6 +1,10 @@
 let secretNumber = document.querySelector("#secretNumber");
 
 const guesNumber = document.querySelector("#guess");
+const check = document.querySelector("#checkBtn");
 secretNumber = Math.ceil(Math.random() * 100);
 console.log(secretNumber);
 console.log(guesNumber);
+console.log(check);
+
+check.onclick = () => {};
