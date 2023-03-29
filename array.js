f71b90a82aa193c0046a4f50a756a2d1ca31dc7a
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-03-29 14:20:03 +0300

    5

---
+++ array.js
@@ -0,0 +1,6 @@
+let secretNumber = document.querySelector("#secretNumber");
+
+const guesNumber = document.querySelector("#guess");
+secretNumber = Math.ceil(Math.random() * 100);
+console.log(secretNumber);
+console.log(guesNumber);
